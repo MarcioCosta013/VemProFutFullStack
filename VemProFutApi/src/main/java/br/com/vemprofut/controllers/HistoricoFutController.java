eec35705c858044cc27665e0e176eab65017ca1a
Marcio Costa <marcio-costa38@hotmail.com>
2025-11-06 13:10:11 -0300

    feat: criacao das classes controllers principais de cada entidade.

---
+++ VemProFutApi/src/main/java/br/com/vemprofut/controllers/HistoricoFutController.java
@@ -0,0 +1,10 @@
+package br.com.vemprofut.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("historico/fut/")
+public class HistoricoFutController {
+
+}

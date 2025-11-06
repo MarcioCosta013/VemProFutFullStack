60713c76d456ed52fd760290cee4172f096d374a
Marcio Costa <marcio-costa38@hotmail.com>
2025-11-05 23:48:02 -0300

    feat: adicionando a Exception personalizada EditorInUseException

---
+++ VemProFutApi/src/main/java/br/com/vemprofut/exceptions/EditorInUseException.java
@@ -0,0 +1,8 @@
+package br.com.vemprofut.exceptions;
+
+public class EditorInUseException extends RuntimeException{
+
+    public EditorInUseException(String message){
+        super(message);
+    }
+}

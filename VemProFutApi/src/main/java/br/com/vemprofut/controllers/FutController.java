29808fce6e71c660537f9d172a881b3ccc975f97
Marcio Costa <marcio-costa38@hotmail.com>
2025-11-14 12:04:33 -0300

    feat(FutController): Adicionar o end ponit delete.

--- VemProFutApi/src/main/java/br/com/vemprofut/controllers/FutController.java
+++ VemProFutApi/src/main/java/br/com/vemprofut/controllers/FutController.java
@@ -6,6 +6,7 @@ import br.com.vemprofut.controllers.response.FutDetailsResponse;
 import br.com.vemprofut.controllers.response.SaveFutResponseDTO;
 import br.com.vemprofut.controllers.response.UpdateFutResponseDTO;
 import br.com.vemprofut.services.IFutService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,22 +22,29 @@ public class FutController {
     private final IFutService futService;
 
     @PostMapping
-    @Transactional
+    @Operation(summary = "Cadastra um novo Fut")
     public ResponseEntity<SaveFutResponseDTO> create (@Valid @RequestBody final SaveFutRequestDTO requestDTO){
         var response = futService.create(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("{id}")
-    @Transactional(readOnly = true)
+    @Operation(summary = "Busca um Fut pelo ID")
     public ResponseEntity<FutDetailsResponse> findByFut (@PathVariable final Long id){
         return ResponseEntity.ok(futService.findById(id));
     }
 
     @PutMapping("{id}")
-    @Transactional
+    @Operation(summary = "Altera um Fut já cadastrado, informando o id")
     public ResponseEntity<UpdateFutResponseDTO> update (@PathVariable final Long id, UpdateFutRequestDTO dto){
         var obj = futService.update(id, dto);
         return ResponseEntity.ok(obj);
+    }
+
+    @DeleteMapping("{id}")
+    @Operation(summary = "Apaga um Fut por meio do id, cuidado! ")
+    public ResponseEntity<Void> delete (@PathVariable final Long id){
+        futService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- VemProFutApi/src/main/java/br/com/vemprofut/repositories/FutRepository.java
+++ VemProFutApi/src/main/java/br/com/vemprofut/repositories/FutRepository.java
@@ -2,12 +2,17 @@ package br.com.vemprofut.repositories;
 
 import br.com.vemprofut.models.FutModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface FutRepository extends JpaRepository<FutModel, Long> {
 
-    Boolean existsByNomeStartingWith(String nomeFut);
+    @Query("""
+            SELECT * FROM fut WHERE nome_fut = :nomeFut
+            """)
+    Boolean existsByNomeStartingWith(@Param("nomeFut") String nomeFut);
 
     Optional<FutModel> findByNome(String nome);
 }

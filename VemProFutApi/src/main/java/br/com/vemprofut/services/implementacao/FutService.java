07bc0b580579420b01052aa09c8fd3fe0f9675d9
Marcio Costa <marcio-costa38@hotmail.com>
2025-11-14 23:56:34 -0300

    fix(FutQueryService): correcao do NullPointerException
    - gerado pelo repository e interferindo nas camadas de Service e QueryService
    - tambem alterei a logica de verificacao do QueryService.

--- VemProFutApi/src/main/java/br/com/vemprofut/services/implementacao/FutService.java
+++ VemProFutApi/src/main/java/br/com/vemprofut/services/implementacao/FutService.java
@@ -4,7 +4,6 @@ import br.com.vemprofut.controllers.request.SaveFutRequestDTO;
 import br.com.vemprofut.controllers.request.UpdateFutRequestDTO;
 import br.com.vemprofut.controllers.response.FutDetailsResponse;
 import br.com.vemprofut.controllers.response.SaveFutResponseDTO;
-import br.com.vemprofut.controllers.response.SaveHistoricoFutResponseDTO;
 import br.com.vemprofut.controllers.response.UpdateFutResponseDTO;
 import br.com.vemprofut.mappers.IFutMapper;
 import br.com.vemprofut.models.DTOs.CartoesDTO;
@@ -18,115 +17,105 @@ import br.com.vemprofut.services.IHistoricoFutService;
 import br.com.vemprofut.services.IPartidasService;
 import br.com.vemprofut.services.query.IFutQueryService;
 import br.com.vemprofut.services.query.IPeladeiroQueryService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 public class FutService implements IFutService {
 
-    @Autowired
-    private IFutQueryService queryService;
+  @Autowired private IFutQueryService queryService;
 
-    @Autowired
-    private IFutMapper mapper;
+  @Autowired private IFutMapper mapper;
 
-    @Autowired
-    private FutRepository repository;
+  @Autowired private FutRepository repository;
 
-    @Autowired
-    private IPartidasService partidasService;
+  @Autowired private IPartidasService partidasService;
 
-    @Autowired
-    private IPeladeiroQueryService peladeiroQueryService;
+  @Autowired private IPeladeiroQueryService peladeiroQueryService;
 
-    @Autowired
-    private IHistoricoFutService historicoFutService;
+  @Autowired private IHistoricoFutService historicoFutService;
 
-    @Override
-    @Transactional
-    public SaveFutResponseDTO create(SaveFutRequestDTO dto) {
-        queryService.verifyNomeFutExist(dto.nome());
-        var peladeiro = peladeiroQueryService.verifyPeladeiroExist(dto.administradorPeladeiroId());
+  @Override
+  @Transactional
+  public SaveFutResponseDTO create(SaveFutRequestDTO dto) {
+    queryService.verifyNomeFutExist(dto.nome());
+    var peladeiro = peladeiroQueryService.verifyPeladeiroExist(dto.administradorPeladeiroId());
 
-        FutModel saved = repository.save(mapper.saveRequestToModel(dto));
+    FutModel saved = repository.save(mapper.saveRequestToModel(dto));
 
-        HistoricoFutModel historico = historicoFutService.create();
-        saved.setHistoricoFutId(historico);
-        saved.setAdministradorPeladeiro(peladeiro);
-        return mapper.toSaveResponse(repository.save(saved));
-    }
+    HistoricoFutModel historico = historicoFutService.create();
+    saved.setHistoricoFutId(historico);
+    saved.setAdministradorPeladeiro(peladeiro);
+    return mapper.toSaveResponse(repository.save(saved));
+  }
 
-    @Override
-    @Transactional(readOnly = true)
-    public FutDetailsResponse findById(Long id) {
-        var futModel= queryService.verifyFutExistRetorn(id);
-        return mapper.modelToDetailsResponse(futModel);
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public FutDetailsResponse findById(Long id) {
+    var futModel = queryService.verifyFutExistRetorn(id);
+    return mapper.modelToDetailsResponse(futModel);
+  }
 
-    @Override
-    @Transactional(readOnly = true)
-    public FutModel findByIdModel(Long id) {
-        return queryService.verifyFutExistRetorn(id);
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public FutModel findByIdModel(Long id) {
+    return queryService.verifyFutExistRetorn(id);
+  }
 
-    @Override
-    @Transactional(readOnly = true)
-    public FutDTO findByNome(String nome) {
-        var futModel = queryService.verifyNomeFutExistRetorn(nome);
-        return mapper.toDTO(futModel);
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public FutDTO findByNome(String nome) {
+    var futModel = queryService.verifyNomeFutExistRetorn(nome);
+    return mapper.toDTO(futModel);
+  }
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<FutDTO> findAll() {
-        return repository.findAll()
-                .stream()
-                .map(mapper::toDTO)
-                .toList();
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public List<FutDTO> findAll() {
+    return repository.findAll().stream().map(mapper::toDTO).toList();
+  }
 
-    @Override
-    @Transactional
-    public UpdateFutResponseDTO update(Long id, UpdateFutRequestDTO dto) {
-        var retorno = queryService.verifyFutExistRetorn(id);
+  @Override
+  @Transactional
+  public UpdateFutResponseDTO update(Long id, UpdateFutRequestDTO dto) {
+    var retorno = queryService.verifyFutExistRetorn(id);
 
-        retorno.setJogadoresPorTime(dto.jogadoresPorTime());
-        retorno.setTempoMaxPartida(dto.tempoMaxPartida());
-        retorno.setMaxGolsVitoria(dto.maxGolsVitoria());
+    retorno.setJogadoresPorTime(dto.jogadoresPorTime());
+    retorno.setTempoMaxPartida(dto.tempoMaxPartida());
+    retorno.setMaxGolsVitoria(dto.maxGolsVitoria());
 
-        return mapper.modelToUpdateResponse(repository.save(retorno));
-    }
+    return mapper.modelToUpdateResponse(repository.save(retorno));
+  }
 
-    @Override
-    @Transactional
-    public void delete(Long id) {
-        queryService.verifyFutExist(id);
-        repository.deleteById(id);
-    }
+  @Override
+  @Transactional
+  public void delete(Long id) {
+    queryService.verifyFutExist(id);
+    repository.deleteById(id);
+  }
 
-    @Override
-    @Transactional
-    public void criarPartida(Boolean jogadoresReservas, FutModel futModel) {
+  @Override
+  @Transactional
+  public void criarPartida(Boolean jogadoresReservas, FutModel futModel) {
 
-        partidasService.create(jogadoresReservas, futModel);
-    }
+    partidasService.create(jogadoresReservas, futModel);
+  }
 
-    //TODO: Adicionar editores
+  // TODO: Adicionar editores
 
-    @Override
-    @Transactional
-    public void addPeladeiro(FutDTO futDTO, PeladeiroDTO peladeiroDTO) {
-        //TODO:Verity
-        futDTO.peladeiros().add(peladeiroDTO.id());
-    }
+  @Override
+  @Transactional
+  public void addPeladeiro(FutDTO futDTO, PeladeiroDTO peladeiroDTO) {
+    // TODO:Verity
+    futDTO.peladeiros().add(peladeiroDTO.id());
+  }
 
-    @Override
-    @Transactional
-    public void addCartoes(FutDTO futDTO, CartoesDTO cartoesDTO) {
-        //TODO:Verity
-        futDTO.cartoes().add(cartoesDTO.id());
-    }
+  @Override
+  @Transactional
+  public void addCartoes(FutDTO futDTO, CartoesDTO cartoesDTO) {
+    // TODO:Verity
+    futDTO.cartoes().add(cartoesDTO.id());
+  }
 }
--- VemProFutApi/src/main/java/br/com/vemprofut/services/query/implementacao/FutQueryService.java
+++ VemProFutApi/src/main/java/br/com/vemprofut/services/query/implementacao/FutQueryService.java
@@ -3,45 +3,52 @@ package br.com.vemprofut.services.query.implementacao;
 import br.com.vemprofut.exceptions.FutInUseException;
 import br.com.vemprofut.exceptions.NomeInUseException;
 import br.com.vemprofut.exceptions.NotFoundException;
-import br.com.vemprofut.models.DTOs.FutDTO;
 import br.com.vemprofut.models.FutModel;
 import br.com.vemprofut.repositories.FutRepository;
 import br.com.vemprofut.services.query.IFutQueryService;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
+@Slf4j
 @Service
+@AllArgsConstructor
 public class FutQueryService implements IFutQueryService {
 
-    FutRepository repository;
+  private final FutRepository repository;
 
-    @Override
-    public void verifyFutExist(Long dto) {
-        if(repository.existsById(dto)){
-            throw new FutInUseException("Fut já cadastrado!");
-        }
+  @Override
+  public void verifyFutExist(Long dto) {
+    if (repository.existsById(dto)) {
+      throw new FutInUseException("Fut já cadastrado!");
     }
+  }
 
-    @Override
-    public FutModel verifyFutExistRetorn(Long fut) {
-        return repository.findById(fut).orElseThrow(
-                () -> new NotFoundException("Não foi encontrado o Fut de id " + fut)
-        );
-    }
+  @Override
+  public FutModel verifyFutExistRetorn(Long fut) {
+    return repository
+        .findById(fut)
+        .orElseThrow(() -> new NotFoundException("Não foi encontrado o Fut de id " + fut));
+  }
 
-    @Override
-    public void verifyNomeFutExist(String nome) {
-        if(repository.existsByNomeStartingWith(nome)){
-            throw new NomeInUseException("Nome de Fut já Cadastrado!");
-        }
+  @Override
+  public void verifyNomeFutExist(String nome) {
+    log.info("Iniciando a verificacao do nome...");
+    boolean exists = repository.existsByNomeStartingWith(nome);
+    if (exists) {
+      log.warn("Nome já existente: " + nome);
+      throw new NomeInUseException("O nome '" + nome + "' já está cadastrado!");
     }
+    log.info("Nome disponível.");
+  }
 
-    @Override
-    public FutModel verifyNomeFutExistRetorn(String nome) {
-        return repository.findByNome(nome).orElseThrow(
-                () -> new EntityNotFoundException("Futebol com nome '" + nome + "' não encontrado")
-        );
-    }
+  @Override
+  public FutModel verifyNomeFutExistRetorn(String nome) {
+    return repository
+        .findByNome(nome)
+        .orElseThrow(
+            () -> new EntityNotFoundException("Futebol com nome '" + nome + "' não encontrado"));
+  }
 }

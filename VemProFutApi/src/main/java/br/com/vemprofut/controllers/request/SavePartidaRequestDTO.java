d5ff67a53e6e683a6593a0f32ac3b27cc619aa0f
Marcio Costa <marcio-costa38@hotmail.com>
2025-11-16 00:32:10 -0300

    refactor(SavePartidaRequestDTO): tirando atributos desnecesarios para criar partidas

--- VemProFutApi/src/main/java/br/com/vemprofut/controllers/request/SavePartidaRequestDTO.java
+++ VemProFutApi/src/main/java/br/com/vemprofut/controllers/request/SavePartidaRequestDTO.java
@@ -4,8 +4,4 @@ import java.util.List;
 
 public record SavePartidaRequestDTO(
         Boolean reservas,
-        Long futId,
-        List<Long> cartoes,
-        List<Long> golsPartida,
-        List<Long> peladeiros
-) {}
+        Long futId) {}
